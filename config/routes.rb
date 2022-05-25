d5c6617e22122d4c36fcdb2ca045286b805d5016
sumanth <sumanthmahilamuri007@gmail.com>
2022-05-25 12:26:17 +0530

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'purchaseorders/index'
-  get 'purchaseorders/show'
-  get 'purchaseorders/delete'
-  get 'purchaseorders/update'
-  get 'purchaseorders/create'
+  resources :purchase_orders
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
