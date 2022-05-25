b906f4b1539062bfa552194694972cda6a2c4196
sumanth <sumanthmahilamuri007@gmail.com>
2022-05-25 15:40:01 +0530

    added create method

--- app/controllers/purchaseorders_controller.rb
+++ app/controllers/purchaseorders_controller.rb
@@ -1,5 +1,9 @@
 class PurchaseordersController < ApplicationController
+  before_action :purchase_params, only: %i[ create ]
+
   def index
+    @purchases = PurchaseOrder.all
+    render :json => @purchases
   end
 
   def show
@@ -12,5 +16,16 @@ class PurchaseordersController < ApplicationController
   end
 
   def create
+    @purchases = PurchaseOrder.new(purchase_params)
+    if @purchases.save
+      render json: @purchases, status: :created
+    else
+      render json: @purchases.errors, status: :unprocessable_entity
+    end
   end
+
+  private
+    def purchase_params
+      params.require(:purchase).permit(:po_number,:delivery_date,:date,:payment_terms,:amount,:cgst,:sgst,:igst,:tds,:description)
+    end
 end
